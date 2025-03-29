cf60c1f154bb010d233946183a2e4ce77129b230
rohitsunilsharma2000 <rohitsunilsharma2000@gmail.com>
2025-03-29 23:16:37 +0530

    JWT Auth Module

--- src/main/java/com/example/supportservice/enums/Priority.java
+++
@@ -1,5 +0,0 @@
-package com.example.supportservice.enums;
-
-public enum Priority {
-    LOW, MEDIUM, HIGH, URGENT
-}
--- src/main/java/com/example/supportservice/enums/TicketStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.supportservice.enums;
-
-public enum TicketStatus {
-    OPEN, IN_PROGRESS, RESOLVED, CLOSED,PENDING
-}
